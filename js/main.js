7ea7d3e39afdf71a72affb2703395914993de90e
y-malbi <malbi@inbox.ru>
2017-05-13 22:46:00 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Modal window MARINA
+$('.title').click(function(){
+  // Подучаем id модального окна которое нужно показать
+  var title_id = $(this).attr('data-target');
+  // $('body').append('<div class="modal-bg"></div>');
+  $('li').removeClass('active-title');
+  $(this).parent().addClass('active-title');
+  $('.tab-content').removeClass('active');
+  $('#'+ title_id).addClass('active');
+});
+
+$('.close-btn').click(function(){
+  $('.modal-bg').remove();
+  $(this).parents('.modal').removeClass('active');
+});
